a723aced0b31c07bddfec056558b968a2cb2bb81
jamesptran <ptranh14@earlham.edu>
2016-11-21 20:08:01 -0500

    Initial Commit

---
+++ Light_bulb/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Light_bulb
+//
+//  Created by James Tran on 11/21/16.
+//  Copyright © 2016 James Tran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
